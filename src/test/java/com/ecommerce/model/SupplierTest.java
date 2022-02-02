1a6e35c71e8ac5753b6bb7ec45f336dcb22027b6
Vinicius Lemes <viniciuslemescaetano@gmail.com>
2022-02-01 21:27:07 -0300

    cria teste de supplier

---
+++ src/test/java/com/ecommerce/model/SupplierTest.java
@@ -0,0 +1,32 @@
+package com.ecommerce.model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SupplierTest {
+
+    @Test
+    public void deveInstanciarSupplierComSucesso() {
+
+        var companyAddres = new Address("Protasio Alves", "385", "Alto Petropolis", "Décimo Andar", "95345-000", "Porto Alegre", "RS");
+        var companyContact = new Contact("RV51@gmail.com", "51944567738");
+        var company = new Supplier(1L, companyContact, companyAddres, "RV51", "22445956000130");
+
+        Assertions.assertEquals(companyAddres, company.getAddress());
+        Assertions.assertEquals(companyContact, company.getContact());
+        Assertions.assertEquals("RV51", company.getTradingName());
+        Assertions.assertEquals("22445956000130", company.getCnpj());
+    }
+    @Test
+    public void mustValidateSupplierWithInvalidInstance() {
+
+        try {
+            new Supplier(1L, null, null, "", "111");
+        } catch (Exception exception) {
+
+            Assertions.assertEquals("[cnpj : CNPJ não pode ser nulo!, tradingName : Nome Fantasia não pode ser nulo!, address : Endereço não pode ser nulo!, contact : Contato não pode ser nulo!, tradingName : Nome Fantasia não pode ser vazio!]", exception.getMessage());
+        }
+    }
+}
