bec1030b74430c64a98d153dee6bfc36f0384ea8
Vinicius Lemes <viniciuslemescaetano@gmail.com>
2022-02-13 03:41:09 -0300

    Altera meotodo de salvar para uso com DTOs

--- src/main/java/com/ecommerce/service/OrderService.java
+++ src/main/java/com/ecommerce/service/OrderService.java
@@ -1,7 +1,10 @@
 package com.ecommerce.service;
 
 import com.ecommerce.infra.repository.OrderRepository;
-import com.ecommerce.model.Order;
+import com.ecommerce.model.Item;
+import com.ecommerce.model.OrderModel;
+import com.ecommerce.service.DTO.ItemDTO;
+import com.ecommerce.service.DTO.OrderDTO;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,14 +14,30 @@ import java.util.Optional;
 public class OrderService {
 
     private OrderRepository orderRepository;
+    private SupplierService supplierService;
+    private CustomerService customerService;
+    private ProductService productService;
 
-    public OrderService(OrderRepository orderRepository) { this.orderRepository = orderRepository; }
+    public OrderService(OrderRepository orderRepository, SupplierService supplierService, CustomerService customerService, ProductService productService) {
+        this.orderRepository = orderRepository;
+        this.supplierService = supplierService;
+        this.customerService = customerService;
+        this.productService = productService;
+    }
 
-    public void registerOrder(Order order) {
+    public void registerOrder(OrderDTO newOrder) {
+        var customer = customerService.searchCustomerById(newOrder.getCustomer());
+        var supplier = supplierService.searchSupplierById(newOrder.getSupplier());
+        var order = new OrderModel(null, customer, supplier, newOrder.getShippingPrice(), null);
+        for(ItemDTO itemDTO : newOrder.getItemDTOList()) {
 
+            var product = productService.searchProductById(itemDTO.getProduct());
+            var item = new Item(null, product, itemDTO.getQuantity());
+            order.addItem(item);
+        }
         orderRepository.save(order);
     }
-    public Optional<Order> searchById(Long id) {
+    public Optional<OrderModel> searchById(Long id) {
 
         if(id == null) {
 
@@ -26,7 +45,7 @@ public class OrderService {
         }
         return orderRepository.findById(id);
     }
-    public List<Order> searchAll() {
+    public List<OrderModel> searchAll() {
 
         return orderRepository.findAll();
     }
